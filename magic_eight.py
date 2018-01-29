94c5d7db49aeb5dcea05d9dfe30e84decd3dd55e
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2018-01-29 15:33:55 -0500

    adding check_question function

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,12 @@
 def ask_eight():
     question = input("What is your question?")
     return question
+    
+def check_question(q):
+    if q[-1] != ?:
+        print("I'm sorry I can only answer questions")
+        q = ask_eight()
+    elif q == "quit"
+        return False
+    else:
+        q = ask_eight()
