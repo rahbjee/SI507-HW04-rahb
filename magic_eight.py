5204191a3e8abf38c12a0b56f4b4d50809987453
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2018-01-29 15:50:53 -0500

    testing

--- magic_eight.py
+++ magic_eight.py
@@ -4,7 +4,7 @@ def ask_eight():
 
 def add_questions():
 
-	lst_answers =  ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", 
+	lst_answers =  ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it",
 	"As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now",
 	"Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 	rand_index = randrange(len(lst_answers))
