7322ff56ff06cb2af78c90e48d183001f136d81f
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2018-01-29 15:58:51 -0500

    Final fix?

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,7 @@
+import random
+
 def ask_eight():
-    question = input("What is your question?")
+    question = input("What is your question? ")
     return check_question(question)
 
 
@@ -8,17 +10,21 @@ def add_questions():
 	lst_answers =  ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it",
 	"As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now",
 	"Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-	rand_index = randrange(len(lst_answers))
+	rand_index = random.randrange(len(lst_answers))
 
 	return lst_answers[rand_index]
 
 
 
 def check_question(q):
-    if q[-1] != '?':
+
+    if q == "quit":
+        print("Thanks for playing!")
+    elif q[-1] != '?':
         print("I'm sorry I can only answer questions")
         q = ask_eight()
-    elif q == "quit"
-        return False
     else:
+        print(add_questions())
         q = ask_eight()
+
+ask_eight()
