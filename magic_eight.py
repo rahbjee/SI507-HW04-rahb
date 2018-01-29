4007377d5ed6f3e3f9a32519900bfe404e7de3b2
Maryam Khodadost <maryamkhodadost@0587393736.wireless.umich.net>
2018-01-29 15:23:53 -0500

    update

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,12 @@
 def ask_eight():
     question = input("What is your question?")
     return question
-print(hi)
+
+def add_questions():
+
+	lst_answers =  ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", 
+	"As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now",
+	"Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+	rand_index = randrange(len(lst_answers))
+
+	return lst_answers[rand_index]
