1d915c8639559492a6be8090c9df99e9f12f12c5
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2018-01-29 15:35:01 -0500

    quick string bug fix

--- magic_eight.py
+++ magic_eight.py
@@ -1,9 +1,9 @@
 def ask_eight():
     question = input("What is your question?")
     return question
-    
+
 def check_question(q):
-    if q[-1] != ?:
+    if q[-1] != '?':
         print("I'm sorry I can only answer questions")
         q = ask_eight()
     elif q == "quit"
